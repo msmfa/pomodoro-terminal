6f4b81ec7af8401a0c9201a71b94085740085035
michael <michaelsydneymoore@hotmail.co>
2023-02-26 18:17:50 +0000

    feat: add pomodoro timer

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::{thread, time};
+
+const WORK_DURATION: u64 = 25 * 60; // 25 minutes in seconds
+const BREAK_DURATION: u64 = 5 * 60; // 5 minutes in seconds
+
+fn main() {
+    let work_duration = time::Duration::from_secs(WORK_DURATION);
+    let break_duration = time::Duration::from_secs(BREAK_DURATION);
+
+    loop {
+        println!("Work for {} minutes...", WORK_DURATION / 60);
+        thread::sleep(work_duration);
+
+        println!("Take a break for {} minutes...", BREAK_DURATION / 60);
+        thread::sleep(break_duration);
+    }
+}
